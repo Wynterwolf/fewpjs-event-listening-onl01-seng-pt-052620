c7df009e1d05e85060f11cda61504b05a0930576
wynterwolf <shinyguns@gmail.com>
2020-11-18 13:16:47 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    // listening node
+    const input = document.getElementById('input');
+    // invocation of addEventListener
+    input.addEventListener('click', function(event) {
+        // callback function
+        alert('I was clicked!');
+});}
